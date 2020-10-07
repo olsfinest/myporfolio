7105e91ca3463549faacf86bb80d4f80469a1472
Shane <swg.sec@gmail.com>
2020-10-07 21:15:31 +0800

    test

--- components/Login.js
+++ components/Login.js
@@ -24,6 +24,7 @@ class Login extends React.Component {
 
         
         var myHeaders = new Headers();
+        
         myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8yY2Y0NDFjNTA0ZmEubmdyb2suaW9cL3JlYWN0cG9yZm9saW93cCIsImlhdCI6MTYwMjA3NDQ3MSwibmJmIjoxNjAyMDc0NDcxLCJleHAiOjE2MDI2NzkyNzEsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.TKJNAqafBZiUGX_4pAtOdYhmoid-I4Z-C3KfBeA6dHQ");
 
         var formdata = new FormData();
